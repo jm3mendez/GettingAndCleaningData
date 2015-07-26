801c9e044c3587cd5705dcd7f9b680e5606ebb67
Jose Mendez <jm3mendez@gmail.com>
2015-07-26 15:41:47 -0400

    the linux script to get and clean

---
+++ linux-script.sh
@@ -0,0 +1,51 @@
+#i.- The data was downloaded, untared and uncompressed by linux 
+#    (extracting phase).
+#    staying in my working Directory
+
+wget "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+tar -xvf "getdata_projectfiles_UCI HAR Dataset.zip"
+mv "getdata_projectfiles_UCI HAR Dataset.zip" getdata_projectfiles_UCI_HAR_Dataset.zip
+#and a new work directory was created.
+
+#ii.- The Data comes noisy, in order to be used adequately I filtered
+#     the extra character contains, like excesive blanks characters, ^M
+#     Characters, excesive carriage return, etc.
+#     At the begining I used the linux sed, like
+#        %sed -e "s/^M//g" myfile > myfile2
+#     but finally I used the linux awk filter in order to expose data
+#     as we need, a columnar aspect. the data row and a carriage return,
+#     the data row and cariage return and so on...
+# generally over the measured files
+#  %awk '{ print$1' }' mytest/mytrain.txt > mytest2/mytrain2.txt
+#the complete shells script was:
+
+# the xxxx_test2 and yyyy_train2 files
+
+(cd  /home/jmendez/Coursera/Getting-And_cleaning_data/Project/UCI_CHAR_Dataset/test;
+ awk -F" " 'BEGIN{cta=0;}{ print $1; cta++ }END{}' X_test.txt > X_test2.txt;
+ cd Inertial*;
+ awk '{print $1}' body_acc_x_test.txt > body_acc_x_test2.txt
+ awk '{print $1}' body_acc_y_test.txt > body_acc_y_test2.txt
+ awk '{print $1}' body_acc_z_test.txt > body_acc_z_test2.txt
+ awk '{print $1}' body_gyro_x_test.txt > body_gyro_x_test2.txt
+ awk '{print $1}' body_gyro_y_test.txt > body_gyro_y_test2.txt
+ awk '{print $1}' body_gyro_z_test.txt > body_gyro_z_test2.txt
+ awk '{print $1}' total_acc_x_test.txt > total_acc_x_test2.txt
+ awk '{print $1}' total_acc_y_test.txt > total_acc_y_test2.txt
+ awk '{print $1}' total_acc_z_test.txt > total_acc_z_test2.txt
+)
+# Train 
+(cd  /home/jmendez/Coursera/Getting-And_cleaning_data/Project/UCI_CHAR_Dataset/train;
+ awk -F" " 'BEGIN{cta=0;}{ print $1; cta++ }END{}' X_train.txt > X_train2.txt;
+ cd Inertial*;
+ awk '{print $1}' body_acc_x_train.txt > body_acc_x_train2.txt
+ awk '{print $1}' body_acc_y_train.txt > body_acc_y_train2.txt
+ awk '{print $1}' body_acc_z_train.txt > body_acc_z_train2.txt
+ awk '{print $1}' body_gyro_x_train.txt > body_gyro_x_train2.txt
+ awk '{print $1}' body_gyro_y_train.txt > body_gyro_y_train2.txt
+ awk '{print $1}' body_gyro_z_train.txt > body_gyro_z_train2.txt
+ awk '{print $1}' total_acc_x_train.txt > total_acc_x_train2.txt
+ awk '{print $1}' total_acc_y_train.txt > total_acc_y_train2.txt
+ awk '{print $1}' total_acc_z_train.txt > total_acc_z_train2.txt
+)
+
